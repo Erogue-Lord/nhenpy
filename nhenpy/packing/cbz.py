253977820283f6ed67d767b3e31fef3b62710f6c
Erogue Lord <maarmota42@gmail.com>
2020-06-06 15:28:48 -0300

    bug fixes

--- nhenpy/packing/cbz.py
+++ nhenpy/packing/cbz.py
@@ -3,6 +3,6 @@ import os
 
 def cbz(folder, name):
     """create an cbz file"""
-    with zipfile.ZipFile(os.path.abspath(f'{folder}/../{name}'), 'w') as zipf:
+    with zipfile.ZipFile(os.path.abspath(f'{folder}/../{os.path.basename(name)}'), 'w') as zipf:
         for filename in os.listdir(folder):
             zipf.write(os.path.abspath(f'{folder}/{filename}'))
